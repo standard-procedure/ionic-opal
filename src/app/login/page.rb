e3fca74ca1db41cb4935ae6804b39a28ca464b4a
Rahoul Baruah <rahoulb@echodek.co>
2023-09-16 18:51:00 +0100

    Rewritten the login page using the new style ruby DSL

--- src/app/login/page.rb
+++ src/app/login/page.rb
@@ -10,17 +10,17 @@ module Login
         dom.ion_content class: "ion-padding" do
           dom.form class: "ion-align-self-stretch" do
             dom.ion_item do
-              dom.ion_input(id: "email", type: "email", label: "Email", "label-placement": "floating", autocomplete: "email", inputmode: "email", value: email, required: true).on "ionChange" do |event|
+              dom.ion_input(id: "email", type: "email", label: "Email", label_placement: "floating", autocomplete: "email", inputmode: "email", value: email, required: true).on "ionChange" do |event|
                 self.email = event.target.value
               end
             end
             dom.ion_item do
-              dom.ion_input(id: "password", label: "Password", "label-placement": "floating", type: "password", autocomplete: "current-password", value: password, required: true, "error-text": error, class: class_map(ion_invalid: error.present?, ion_touched: error.present?)).on "ionChange" do |event|
+              dom.ion_input(id: "password", label: "Password", label_placement: "floating", type: "password", autocomplete: "current-password", value: password, required: true, "error-text": error, class: class_map(ion_invalid: error.present?, ion_touched: error.present?)).on "ionChange" do |event|
                 self.password = event.target.value
               end
             end
-            dom.e "ion-item" do
-              dom.e "ion-button", type: "submit", color: "success", size: "medium", slot: "end" do
+            dom.ion_item do
+              dom.ion_button type: "submit", color: "success", size: "medium", slot: "end" do
                 "Log in"
               end
             end
