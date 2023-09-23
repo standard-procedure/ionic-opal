a7b418e236c2310a9397c1a6298c5a50509b2375
Rahoul Baruah <rahoulb@echodek.co>
2023-09-23 17:20:29 +0100

    More data on the assessments page

--- src/app/assessments/page.rb
+++ src/app/assessments/page.rb
@@ -5,11 +5,25 @@ class Assessments
     property :account_id, type: :integer
     property :assessment_id, type: :integer
     property :title
+    property :starting_on, tyoe: :date
+    property :ending_on, tyoe: :date
+    property :status
 
     def render
       inner_dom do |dom|
         dom.ui_header title: title
         dom.ion_content class: "ion-padding" do
+          dom.ion_card do
+            dom.ion_card_header do
+              dom.ion_card_title { assessment.title }
+            end
+
+            dom.ion_card_content do
+              dom.p { assessment.starting_on }
+              dom.p { assessment.ending_on }
+              dom.p { assessment.status }
+            end
+          end
           dom.candidates_list.created do |l|
             l.assessment = assessment
             l.account = account
